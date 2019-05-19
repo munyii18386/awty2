b596b6ebd96e3c2977483553a791321845dd04f9
Lilibug <munyii18386@hotmail.com>
2019-05-19 16:48:19 -0700

    final

--- Awty/app/src/main/java/edu/washington/lmburu/awty/MainActivity.kt
+++ Awty/app/src/main/java/edu/washington/lmburu/awty/MainActivity.kt
@@ -16,76 +16,85 @@ import android.util.Log
 import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.fragment_toast.*
+
 
 private const val TAG = "main"
 
 class MainActivity : AppCompatActivity() {
     private var alarmMgr: AlarmManager? = null
     private lateinit var alarmIntent: PendingIntent
-//    private var message:Editable? = null
-//    private  var phoneNumber:Editable? = null
-//    private var alarmInterval:Editable? = null
-
-
+    private  var givenMsg = ""
+    private  var givenPhoneNum = 0
+    private var givenInterval = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 //      get the Alarm Manager
         alarmMgr = getSystemService(Context.ALARM_SERVICE) as AlarmManager
-//        create pending intent
-        alarmIntent = Intent(this, MyAlarm::class.java).let { intent ->
-            PendingIntent.getBroadcast(this, 0, intent, 0)
-        }
-//      prevents the boot receiver from being called unnecessarily
-        val receiver = ComponentName(this, MyAlarm::class.java)
-        this.packageManager.setComponentEnabledSetting(
-            receiver,
-            PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
-            PackageManager.DONT_KILL_APP)
+        tv_btn.isEnabled = false
+        tv_phone.isEnabled = false
+        tv_interval.isEnabled = false
+
+        tv_msg.addTextChangedListener(object: TextWatcher{
+            override fun afterTextChanged(s: Editable?) {
+                tv_phone.isEnabled = !tv_msg.text.isBlank()
+                givenMsg = tv_msg.text.toString()
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        })
+
+        tv_phone.addTextChangedListener(object: TextWatcher{
+            override fun afterTextChanged(s: Editable?) {
+                givenPhoneNum = tv_phone.text.toString().toInt()
+                tv_interval.isEnabled = !tv_phone.text.isBlank()
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        })
 
 
+        tv_interval.addTextChangedListener(object: TextWatcher{
+            override fun afterTextChanged(s: Editable?) {
+                if(!tv_interval.text.toString().startsWith("0")){
+                    givenInterval = tv_interval.text.toString().toInt()
+                    tv_btn.isEnabled = !tv_interval.text.isBlank()
+                }else{
+                    Toast.makeText(this@MainActivity, "Interval has to be greater than zero", Toast.LENGTH_LONG).show()
+                }
+            }
 
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 
-//        tv_interval.addTextChangedListener(object: TextWatcher{
-//            override fun afterTextChanged(s: Editable?) {  }
-//
-//            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-//
-//            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
-//        })
-
-//
-        val interval:Long = 2 * 1000
-        val currentTime = System.currentTimeMillis()
-
-
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        })
 
 
         tv_btn.setOnClickListener {
-
-//            on click intiate alarm
-            alarmMgr?.setRepeating(AlarmManager.RTC_WAKEUP, currentTime+ interval, interval, alarmIntent)
-
-            Toast.makeText(this, "intiated", Toast.LENGTH_SHORT).show()
-            Log.i(TAG, "This alarm has been fired at: $currentTime")
-            tv_btn.text = "STOP"
-//            btn.isClickable = !(msg == "" || phone.isNullOrBlank() || minutes.isNullOrBlank())
+            val interval:Long = givenInterval.toLong() * 1000 * 60
+            val currentTime = System.currentTimeMillis()
+//            create pending intent
+            alarmIntent = Intent(this, MyAlarm::class.java)
+                .putExtra("message", givenMsg)
+                .putExtra("phone", givenPhoneNum.toString())
+                .let { intent ->
+                    PendingIntent.getBroadcast(this, 0, intent, 0)
+                }
+            if(tv_btn.text == "START"){
+                alarmMgr?.setRepeating(AlarmManager.RTC_WAKEUP, currentTime+ interval, interval, alarmIntent)
+                Toast.makeText(this, "Alarm has been set", Toast.LENGTH_SHORT).show()
+                tv_btn.text = "STOP"
+            } else{
+                alarmMgr?.cancel(alarmIntent)
+                Toast.makeText(this, "Alarm has been stopped", Toast.LENGTH_SHORT).show()
+                tv_btn.text = "START"
+            }
         }
-
-//        val toast = ToastFragment.newInstance()
-//        val ft = supportFragmentManager.beginTransaction()
-//        ft.replace(R.id.container, toast)
-//        ft.addToBackStack(null)
-//        ft.commit()
-
     }
-
-    fun inputIsValid(){
-
-    }
-
-
 }
 
--- Awty/app/src/main/java/edu/washington/lmburu/awty/MyAlarm.kt
+++ Awty/app/src/main/java/edu/washington/lmburu/awty/MyAlarm.kt
@@ -7,7 +7,7 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.widget.Toast
-import kotlinx.android.synthetic.main.fragment_toast.*
+
 
 private const val TAG = "alarm"
 class MyAlarm: BroadcastReceiver() {
@@ -15,8 +15,10 @@ class MyAlarm: BroadcastReceiver() {
 
     override fun onReceive(context: Context?, intent: Intent?) {
 
-        Toast.makeText(context, "fired", Toast.LENGTH_SHORT).show()
-        Log.i(TAG, "awty")
+        val givenMsg = intent?.getStringExtra("message")
+        val givenPhoneNum = intent?.getStringExtra("phone")
+
+        Toast.makeText(context, "$givenPhoneNum: Are We There Yet?", Toast.LENGTH_SHORT).show()
 
 
     }
