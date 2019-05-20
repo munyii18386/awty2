8bd027ae115f6de813c69283ab5a5c11399f21cf
Lilibug <munyii18386@hotmail.com>
2019-05-19 20:45:37 -0700

    screenshots added

--- Awty/app/src/main/java/edu/washington/lmburu/awty/MainActivity.kt
+++ Awty/app/src/main/java/edu/washington/lmburu/awty/MainActivity.kt
@@ -6,10 +6,12 @@ import android.app.PendingIntent
 import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.content.pm.PackageManager
 import android.icu.util.DateInterval
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.ContextCompat.getSystemService
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
@@ -21,15 +23,26 @@ import kotlinx.android.synthetic.main.activity_main.*
 private const val TAG = "main"
 
 class MainActivity : AppCompatActivity() {
-    private var alarmMgr: AlarmManager? = null
-    private lateinit var alarmIntent: PendingIntent
-    private  var givenMsg = ""
-    private  var givenPhoneNum = 0
-    private var givenInterval = 0
+
+    companion object{
+        private lateinit var sharedPreferences: SharedPreferences
+        private var alarmMgr: AlarmManager? = null
+        private lateinit var alarmIntent: PendingIntent
+        private  var givenMsg = ""
+        private  var givenPhoneNum: Editable? = null
+        private var givenInterval = 0
+        private const val PREF = "pref"
+        private const val MSG ="msg"
+        private const val PHONE_NUM = "phone"
+        private const val INTERVAL = "interval"
+
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        sharedPreferences = getSharedPreferences(PREF, Context.MODE_PRIVATE)
 //      get the Alarm Manager
         alarmMgr = getSystemService(Context.ALARM_SERVICE) as AlarmManager
         tv_btn.isEnabled = false
@@ -49,7 +62,6 @@ class MainActivity : AppCompatActivity() {
 
         tv_phone.addTextChangedListener(object: TextWatcher{
             override fun afterTextChanged(s: Editable?) {
-                givenPhoneNum = tv_phone.text.toString().toInt()
                 tv_interval.isEnabled = !tv_phone.text.isBlank()
             }
 
@@ -62,7 +74,6 @@ class MainActivity : AppCompatActivity() {
         tv_interval.addTextChangedListener(object: TextWatcher{
             override fun afterTextChanged(s: Editable?) {
                 if(!tv_interval.text.toString().startsWith("0")){
-                    givenInterval = tv_interval.text.toString().toInt()
                     tv_btn.isEnabled = !tv_interval.text.isBlank()
                 }else{
                     Toast.makeText(this@MainActivity, "Interval has to be greater than zero", Toast.LENGTH_LONG).show()
@@ -76,8 +87,16 @@ class MainActivity : AppCompatActivity() {
 
 
         tv_btn.setOnClickListener {
+            givenPhoneNum = tv_phone.text
+            givenInterval = tv_interval.text.toString().toInt()
             val interval:Long = givenInterval.toLong() * 1000 * 60
             val currentTime = System.currentTimeMillis()
+            sharedPreferences
+                .edit()
+                .putLong(INTERVAL, interval)
+                .putString(PHONE_NUM, givenPhoneNum.toString())
+                .apply()
+
 //            create pending intent
             alarmIntent = Intent(this, MyAlarm::class.java)
                 .putExtra("message", givenMsg)
@@ -86,11 +105,14 @@ class MainActivity : AppCompatActivity() {
                     PendingIntent.getBroadcast(this, 0, intent, 0)
                 }
             if(tv_btn.text == "START"){
+
                 alarmMgr?.setRepeating(AlarmManager.RTC_WAKEUP, currentTime+ interval, interval, alarmIntent)
+
                 Toast.makeText(this, "Alarm has been set", Toast.LENGTH_SHORT).show()
                 tv_btn.text = "STOP"
             } else{
                 alarmMgr?.cancel(alarmIntent)
+
                 Toast.makeText(this, "Alarm has been stopped", Toast.LENGTH_SHORT).show()
                 tv_btn.text = "START"
             }
--- Awty/app/src/main/java/edu/washington/lmburu/awty/MyAlarm.kt
+++ Awty/app/src/main/java/edu/washington/lmburu/awty/MyAlarm.kt
@@ -1,6 +1,8 @@
 package edu.washington.lmburu.awty
 
+import android.app.AlarmManager
 import android.app.Application
+import android.app.PendingIntent.getBroadcast
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
@@ -11,14 +13,28 @@ import android.widget.Toast
 
 private const val TAG = "alarm"
 class MyAlarm: BroadcastReceiver() {
-
+    companion object{
+        private const val PREF = "pref"
+        private const val MSG ="msg"
+        private const val PHONE_NUM = "phone"
+        const val INTERVAL = "interval"
+        private var alarmMgr: AlarmManager? = null
+    }
 
     override fun onReceive(context: Context?, intent: Intent?) {
-
-        val givenMsg = intent?.getStringExtra("message")
         val givenPhoneNum = intent?.getStringExtra("phone")
-
         Toast.makeText(context, "$givenPhoneNum: Are We There Yet?", Toast.LENGTH_SHORT).show()
+//        Log.i(TAG, "next round")
+
+        if (intent?.action == "android.intent.action.BOOT_COMPLETED") {
+            // Set the alarm here.
+            val data = context?.getSharedPreferences(PREF, Context.MODE_PRIVATE)
+            val storedInterval = data!!.getLong(INTERVAL, 0)
+            alarmMgr = context?.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+            val pendingIntent = getBroadcast(context, 0, intent, 0)
+            alarmMgr?.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis()+ storedInterval, storedInterval, pendingIntent)
+//            Log.i(TAG, "reboot")
+        }
 
 
     }
